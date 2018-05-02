17d9a68df7d336ac9749c4d99fc2d375cfbba023
Mike King <michael.king@zeromass.co.uk>
2018-05-02 11:22:33 +0100

    updated comments

--- monitoring/views.py
+++ monitoring/views.py
@@ -23,15 +23,15 @@ def upload_csv(request):
 def logins_by_server(request):
 	# fairly lightweight data so get all server login data to minimise DB round-trips
 	# can use javascript locally to filter if necessary plus will give user a more
-	# responsive feel as we will not have network issues to contend with after first 
+	# responsive feel as we will not have network issues to contend with after initial 
 	# data fetch
 
 	# Would probably use an ORM here, but rough SQL...
-	# select s.server_name, s.server_ip, l.login_date, u.username, login l from logins l where l.date >= '2017-01-01' and l.date <='2018-04-30'
+	# select s.server_name, s.server_ip, l.login_date, u.username, from logins l where l.date >= '2017-01-01' and l.date <='2018-04-30'
 	# join server s on l.server_id = s.server_id
 	# join user u on l.user_id = u.user_id
 	
-	# need to think about a fail-safe limit on the number of rows returned - say, 10k
+	# TODO: think about a fail-safe limit on the number of rows returned - say, 10k
 	
 	# build JSON data from DB data...
 	# the client must render the data correctly and create filter controls etc
